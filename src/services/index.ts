80402c24a3b11e6f1a9a6b0a32b6aa1e1c6f5411
fermelli <luisfernandosalgadomiguez@gmail.com>
2023-07-11 17:09:52 -0400

    eliminar interface que no se usa

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,7 @@
-import productsService from "./products.service";
 import clientesService from "./clientes.service";
 import equiposService from "./equipos.service";
 import ordenesTrabajoService from "./ordenes-trabajo.service";
 
-export { productsService };
 export { clientesService };
 export { equiposService };
 export { ordenesTrabajoService };
